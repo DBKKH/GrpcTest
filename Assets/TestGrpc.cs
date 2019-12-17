68838a463af299c66a750a0e67de769aa876018d
koike <hdkkik@gmail.com>
2019-12-17 18:28:45 +0900

    organize
    Signed-off-by: koike <hdkkik@gmail.com>

---
+++ Assets/TestGrpc.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Grpc.Core;
+
+public class TestGrpc : MonoBehaviour
+{
+    Channel channel;
+    void Start()
+    {        
+
+    }
+}
